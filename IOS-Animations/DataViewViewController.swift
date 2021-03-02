b9015d62e43eee04c17ec63f2d2604fa46142b8e
Sebastian Otto <sebastianotto.koeln@outlook.de>
2021-03-02 17:28:26 +0100

    PageViewController working

---
+++ IOS-Animations/DataViewViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewViewController.swift
+//  IOS-Animations
+//
+//  Created by Sebastian Otto on 02.03.21.
+//
+
+import UIKit
+
+class DataViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- IOS-Animations/ViewController.swift
+++ IOS-Animations/ViewController.swift
@@ -7,13 +7,82 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIPageViewController, UIPageViewControllerDataSource {
+
+    lazy var viewControllerList:[UIViewController] = {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        return [
+        sb.instantiateViewController(identifier: "SimpleAnimations"),
+        sb.instantiateViewController(identifier: "AnimatiosnWithOptions")
+        ]
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.dataSource = self
+        
+        if let firstViewController = viewControllerList.first {
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        for view in self.view.subviews {
+            if view is UIScrollView {
+                view.frame = UIScreen.main.bounds
+            } else if (view is UIPageControl) {
+                view.backgroundColor = UIColor.clear
+            }
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = viewControllerList.firstIndex(of: viewController) else {
+           return nil
+        }
+        
+        let prevIndex = viewControllerIndex - 1
+        
+        guard prevIndex >= 0 else {
+            return viewControllerList.last
+        }
+        
+        guard viewControllerList.count > prevIndex else {
+            return nil
+        }
+        
+        return viewControllerList[prevIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = viewControllerList.firstIndex(of: viewController) else {
+           return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < viewControllerList.count else {
+            return viewControllerList.first
+        }
+        
+        guard viewControllerList.count > nextIndex else {
+            return nil
+        }
+        
+        return viewControllerList[nextIndex]
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return viewControllerList.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = viewControllerList.firstIndex(of: firstViewController) else {
+            return 0
+        }
+        
+        return firstViewControllerIndex
+    }
 }
-
