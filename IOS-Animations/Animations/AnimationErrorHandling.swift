721cd0083b7db53d5ead644a7eced62ba2402bcb
Sebastian Otto <sebastianotto.koeln@outlook.de>
2021-03-04 18:44:51 +0100

    added "animation with closure" page

--- IOS-Animations/Animations/AnimationErrorHandling.swift
+++ IOS-Animations/Animations/AnimationErrorHandling.swift
@@ -8,22 +8,58 @@
 import UIKit
 
 class AnimationErrorHandling: UIViewController {
+    
+    @IBOutlet weak var withBool: UIView!
+    @IBOutlet weak var withoutBool: UIView!
+    
+    var originX:CGFloat = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        originX = withBool.center.x
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func animationWithBool(view:UIView) {
+        UIView.animate(withDuration: 10, animations: {
+            view.center.x += 200
+        }, completion: {isFinished in
+            if isFinished {
+                view.center.x = self.originX
+            }
+        })
     }
-    */
-
+    
+    private func animationWithoutBool(view:UIView) {
+        UIView.animate(withDuration: 10, animations: {
+            view.center.x += 200
+        }, completion: {_ in
+                view.center.x = self.originX
+        })
+    }
+    
+    @IBAction func onClickBool(_ sender: Any) {
+        animationWithBool(view: self.withBool)
+    }
+    
+    @IBAction func onClickNoBool(_ sender: Any) {
+        animationWithoutBool(view: self.withoutBool)
+    }
+    
+    @IBAction func onClickStartAll(_ sender: Any) {
+        animationWithBool(view: self.withBool)
+        animationWithoutBool(view: self.withoutBool)
+    }
+    
+    @IBAction func onClickStop(_ sender: Any) {
+        withBool.layer.removeAllAnimations()
+        withoutBool.layer.removeAllAnimations()
+    }
+    
+    @IBAction func onClickReset(_ sender: Any) {
+        self.withBool.center.x = originX
+        self.withoutBool.center.x = originX
+    }
+    
+    
 }
